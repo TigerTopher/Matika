46cd13fd3c286b203804d8c3f139c87c1c31a824
Cyan Vee <Cyanvillelite@users.noreply.github.com>
2015-05-22 08:26:54 +0800

    Update try1.py

--- try1.py
+++ try1.py
@@ -12,10 +12,6 @@ def p_statementTop(p):
   '''statementTop : END
                   | statement'''
 
-def p_statement(p):
-    '''statement : declaration statementTop
-                | expression statementTop'''
-
 # # # # # # # # # C Y A N # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 def p_statement(p):
@@ -34,8 +30,8 @@ def p_statement(p):
                 | FunctionCall statementTop
                 | returnDec statementTop
                 | stringOpStatement statementTop
-                | read t_LPAREN IDENTIFIER t_RPAREN t_SEMICOLON statementTop
-                | print t_LPAREN content t_RPAREN t_SEMICOLON statementTop'''              
+                | read LPAREN IDENTIFIER RPAREN SEMICOLON statementTop
+                | print LPAREN content RPAREN SEMICOLON statementTop'''              
 
 def p_statement1(p):
     '''statement1 : declaration
@@ -53,8 +49,8 @@ def p_statement1(p):
                 | FunctionCall
                 | returnDec
                 | stringOpStatement
-                | read t_LPAREN IDENTIFIER t_RPAREN t_SEMICOLON
-                | print t_LPAREN content t_RPAREN t_SEMICOLON'''
+                | read LPAREN IDENTIFIER RPAREN SEMICOLON
+                | print LPAREN content RPAREN SEMICOLON'''
 
 def p_booleanValue(p):
     '''booleanValue : TRUE
@@ -64,11 +60,11 @@ def p_content(p):
     '''content : toPrint morePrint'''
 
 def p_morePrint(p):
-    '''morePrint : t_PLUS toPrint morePrint
-                | t_EMPTY'''
+    '''morePrint : PLUS toPrint morePrint
+                | EMPTY'''
 
 def p_toPrint(p):
-    '''toPrint : t_QUOTE string t_QUOTE
+    '''toPrint : QUOTE string QUOTE
               | number
               | IDENTIFIER'''
 
@@ -78,10 +74,10 @@ def p_declaration(p):
                   | unionDec'''
 
 def p_identifierDeclaration(p):
-    '''identifierDeclaration : dataType IDENTIFIER t_SEMICOLON
-                            | dataType IDENTIFIER t_LBRACK CONSTANT t_RBRACK t_SEMICOLON
-                            | dataType IDENTIFIER t_LBRACK CONSTANT t_RBRACK t_EQUAL expression t_SEMICOLON
-                            | dataType IDENTIFIER t_EQUAL expression t_SEMICOLON'''
+    '''identifierDeclaration : dataType IDENTIFIER SEMICOLON
+                            | dataType IDENTIFIER LBRACK CONSTANT RBRACK SEMICOLON
+                            | dataType IDENTIFIER LBRACK CONSTANT RBRACK EQUAL expression SEMICOLON
+                            | dataType IDENTIFIER EQUAL expression SEMICOLON'''
 
 def p_dataType(p):
   '''dataType : INT
@@ -93,8 +89,8 @@ def p_dataType(p):
   print p[1]
 
 def p_stringOPStatement(p):
-    '''stringOpStatement : IDENTIFIER stringOP t_LPAREN string t_RPAREN t_SEMICOLON
-                        | IDENTIFIER stringOp t_LPAREN IDENTIFIER t_RPAREN t_SEMICOLON'''
+    '''stringOpStatement : IDENTIFIER stringOP LPAREN string RPAREN SEMICOLON
+                        | IDENTIFIER stringOp LPAREN IDENTIFIER RPAREN SEMICOLON'''
 
 def p_stringOp(p):
     '''stringOp : SPLIT
@@ -103,57 +99,57 @@ def p_stringOp(p):
                 | COPY'''
 
 def p_listDec(p):
-    '''listDec : IDENTIFIER t_EQUAL list t_SEMICOLON'''
+    '''listDec : IDENTIFIER EQUAL list SEMICOLON'''
 
 def p_list(p):
-    '''list : t_LBRACK listElem t_RBRACK'''
+    '''list : LBRACK listElem RBRACK'''
 
 def p_listElem(p):
     '''listElem : validListUnionValues
-                | validListUnionValues t_COMMA listElem
-                | t_EMPTY'''
+                | validListUnionValues COMMA listElem
+                | EMPTY'''
 
 def p_listEval(p):
-    '''listEval : IDENTIFIER t_LBRACK CONSTANT t_RBRACK'''
+    '''listEval : IDENTIFIER LBRACK CONSTANT RBRACK'''
 
 def p_unionDec(p):
-    '''unionDec : IDENTIFIER t_EQUAL uninon t_SEMICOLON'''
+    '''unionDec : IDENTIFIER EQUAL union SEMICOLON'''
 
 def p_union(p):
-    '''union : t_LCURLY unionElement t_RCURLY'''
+    '''union : LCURLY unionElement RCURLY'''
 
 def p_unionElement(p):
-    '''unionElement : string t_EQUAL validListUnionValues
-                  | string t_EQUAL validListUnionValues t_COMMA unionElement
-                  | t_EMPTY'''
+    '''unionElement : string EQUAL validListUnionValues
+                  | string EQUAL validListUnionValues COMMA unionElement
+                  | EMPTY'''
 
 def p_unionAdd(p):
-    '''unionAdd : IDENTIFIER t_LBRACK string t_RBRACK t_EQUAL validListUnionValues t_SEMICOLON'''
+    '''unionAdd : IDENTIFIER LBRACK string RBRACK EQUAL validListUnionValues SEMICOLON'''
 
 def p_validListUnionValues(p):
     '''validListUnionValues : intValue
                           | longValue
                           | floatValue
                           | doubleValue
-                          | charValue
+                          | CHARVALUE
                           | booleanValue
-                          | stringValue
+                          | STRINGVALUE
                           | listValue'''
 
 def p_unionKeys(p):
-    '''unionKeys : IDENTIFIER KEYS t_SEMICOLON'''
+    '''unionKeys : IDENTIFIER KEYS SEMICOLON'''
 
 def p_unionEval(p):
-    '''unionEval : IDENTIFIER t_ARROW string'''
+    '''unionEval : IDENTIFIER ARROW string'''
 
 def p_typeCastToInt(p):
-    '''typeCastToInt : TC_INT IDENTIFIER t_SEMICOLON'''
+    '''typeCastToInt : TC_INT IDENTIFIER SEMICOLON'''
 
 def p_typeCastToFloat(p):
-    '''typecastToFloat : TC_FLOAT IDENTIFIER t_SEMICOLON'''
+    '''typecastToFloat : TC_FLOAT IDENTIFIER SEMICOLON'''
 
 def p_WhileLoop(p):
-    '''WhileLoop : WHILE Condition t_COLON Body'''
+    '''WhileLoop : WHILE Condition COLON Body'''
 
 def p_Body(p):
     '''Body : statementMore'''
@@ -166,36 +162,36 @@ def p_statementMoreCont(p):
     '''statementMoreCont : statement1 statementMore'''
 
 def p_ForLoop(p):
-    '''ForLoop : for t_LPAREN AssignmentHead Condition Increment t_RPAREN Body'''
+    '''ForLoop : for LPAREN AssignmentHead Condition Increment RPAREN Body'''
 
 def p_DoWhileLoop(p):
-    '''DoWhileLoop : DO t_COLON Body WHILE Condition t_SEMICOLON'''
+    '''DoWhileLoop : DO COLON Body WHILE Condition SEMICOLON'''
 
 def p_Increment(p):
-    '''Increment : t_SEMICOLON Operand Options'''
+    '''Increment : SEMICOLON Operand Options'''
 
 def p_Options(p):
     '''Options : Iterator
               | AssignmentOperator Operand'''
 
 def p_IfThenElse(p):
-    '''IfThenElse : IF Condition t_COLON Body addElif'''
+    '''IfThenElse : IF Condition COLON Body addElif'''
 
 def p_addElif(p):
     '''addElif : addElif2
               | FI
-              | ELSE t_COLON Body'''
+              | ELSE COLON Body'''
 
 def p_addElif2(p):
     '''addElif2 : elifClause addElif'''
 
 def p_elifClause(p):
-    '''elifClause : ELIF Condition t_COLON Body'''
+    '''elifClause : ELIF Condition COLON Body'''
 
 def p_Condition(p):
     '''Condition : RelationExpression
-                | t_LPAREN Condition Compound Condition t_RPAREN
-                | t_LPAREN NOT Condition t_RPAREN'''
+                | LPAREN Condition Compound Condition RPAREN
+                | LPAREN NOT Condition RPAREN'''
 
 def p_Operand(p):
     '''Operand : IDENTIFIER
@@ -207,15 +203,15 @@ def p_Compound(p):
               | OR'''
 
 def p_Iterator(p):
-    '''Iterator : t_PLUSPLUS
-              | t_MINUSMINUS
-              | t_EMPTY'''
+    '''Iterator : PLUSPLUS
+              | MINUSMINUS
+              | EMPTY'''
 
 def p_AssignmentHead(p):
-    '''AssignmentHead : t_BAR IDENTIFIER AssignmentOption t_BAR t_SEMICOLON'''
+    '''AssignmentHead : BAR IDENTIFIER AssignmentOption BAR SEMICOLON'''
 
 def p_AssignmentOption(p):
-    '''AssignmentOption : t_EQUAL AssignmentOptionChain
+    '''AssignmentOption : EQUAL AssignmentOptionChain
                       | AssignmentOperator AssignmentOptions2'''
 
 def p_AssignmentOptions2(p):
@@ -229,11 +225,11 @@ def p_AssignmentOptionChain(p):
                               | AssignmentOptions2'''
 
 def p_AssignmentOperator(p):
-    '''AssignmentOperator : t_PLUSEQUAL
-                        | t_MINUSEQUAL
-                        | t_MULTEQUAL
-                        | t_DIVEQUAL
-                        | t_MODEQUAL'''
+    '''AssignmentOperator : PLUSEQUAL
+                        | MINUSEQUAL
+                        | MULTEQUAL
+                        | DIVEQUAL
+                        | MODEQUAL'''
 
 def p_expression(p):
     '''expression : ArithmeticExpression
@@ -243,50 +239,50 @@ def p_expression(p):
                   | RelationExpression'''
 
 def p_RelationExpression(p):
-    '''RelationExpression : t_LPAREN Operand RelationOperator Operand t_RPAREN'''
+    '''RelationExpression : LPAREN Operand RelationOperator Operand RPAREN'''
 
 def p_ArithmeticExpression(p):
-    '''ArithmeticExpression : t_LESSTHAN Operand ArithmeticOperator Operand t_GREATERTHAN'''
+    '''ArithmeticExpression : LESSTHAN Operand ArithmeticOperator Operand GREATERTHAN'''
 
 def p_ArithmeticOperator(p):
-    '''ArithmeticOperator : t_PLUS
-                          | t_MINUS
-                          | t_MULT
-                          | t_DIV
-                          | t_MOD'''
+    '''ArithmeticOperator : PLUS
+                          | MINUS
+                          | MULT
+                          | DIV
+                          | MOD'''
 
 def p_RelationOperator(p):
-    '''RelationOperator : t_EQUALEQUAL
-                        | t_EQUAL
-                        | t_LESSTHAN
-                        | t_GREATERTHAN
-                        | t_GREATEREQ
-                        | t_LESSEREQ
-                        | t_NOTEQ'''
+    '''RelationOperator : EQUALEQUAL
+                        | EQUAL
+                        | LESSTHAN
+                        | GREATERTHAN
+                        | GREATEREQ
+                        | LESSEREQ
+                        | NOTEQ'''
 
 def p_FunctionDefinition(p):
-    '''p_FunctionDefinition : dataType Function t_COLON Body'''
+    '''p_FunctionDefinition : dataType Function COLON Body'''
 
 def p_Function(p):
-    '''Function : IDENTIFIER t_LPAREN Parameter t_RPAREN'''
+    '''Function : IDENTIFIER LPAREN Parameter RPAREN'''
 
 def p_Parameter(p):
     ''' Parameter : dataType IDENTIFIER
-                  | dataType IDENTIFIER t_COMMA Parameter
-                  | t_EMPTY'''
+                  | dataType IDENTIFIER COMMA Parameter
+                  | EMPTY'''
 
 def p_FunctionCall(p):
-    ''' FunctionCall : IDENTIFIER t_LPAREN FunctionCallParameter t_RPAREN t_SEMICOLON'''
+    ''' FunctionCall : IDENTIFIER LPAREN FunctionCallParameter RPAREN SEMICOLON'''
 
 def p_FunctionCallParameter(p):
     '''FunctionCallParameter : IDENTIFIER
-                            | IDENTIFIER t_COMMA p_FunctionCallParameter
-                            | t_EMPTY'''
+                            | IDENTIFIER COMMA p_FunctionCallParameter
+                            | EMPTY'''
 
 def p_returnDec(p):
-    '''returnDec : RETURN CONSTANT t_SEMICOLON
-                | RETURN IDENTIFIER t_SEMICOLON
-                | RETURN expression t_SEMICOLON'''
+    '''returnDec : RETURN CONSTANT SEMICOLON
+                | RETURN IDENTIFIER SEMICOLON
+                | RETURN expression SEMICOLON'''
 
 
  # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
